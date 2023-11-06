c966cc37893ec674e479f461d74f7acae63f73f8
xszhu522 <919317559@qq.com>
2023-11-06 18:21:09 +0800

    添加网站运行时间

---
+++ docs/.vuepress/client.ts
@@ -0,0 +1,14 @@
+import { defineClientConfig } from "@vuepress/client";
+import { setupRunningTimeFooter } from "vuepress-theme-hope/presets/footerRunningTime.js";
+
+export default defineClientConfig({
+    setup() {
+        setupRunningTimeFooter(
+            new Date("2023-11-05"),
+            {
+                "/": "已运行 :day 天 :hour 小时 :minute 分钟 :second 秒",
+            },
+            true,
+        );
+    },
+});
